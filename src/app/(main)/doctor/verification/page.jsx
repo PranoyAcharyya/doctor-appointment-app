26a7c2f4b8c74cca72cd65b0daf228a67235e010
PranoyAcharyya <acharyapranoy@gmail.com>
2026-04-01 02:00:50 +0530

    push

--- src/app/(main)/doctor/verification/page.jsx
+++ src/app/(main)/doctor/verification/page.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
-import { getCurrentUser } from '../../../../../actions/onBoarding';
-import { redirect } from 'next/navigation';
-
+import React from "react";
+import { getCurrentUser } from "../../../../../actions/onBoarding";
+import { redirect } from "next/navigation";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ClipboardCheck, XCircle } from "lucide-react";
 
 const VerificationPage = async () => {
-    const user = await getCurrentUser();
-    if(user?.verificationStatus === "VERIFIED"){
-        redirect("/doctor");
-    }
-    const isRejected = user?.verificationStatus === "REJECTED";
-    
-  return (
-    <div>page</div>
-  )
-}
+  const user = await getCurrentUser();
+  if (user?.verificationStatus === "VERIFIED") {
+    redirect("/doctor");
+  }
+  const isRejected = user?.verificationStatus === "REJECTED";
 
-export default VerificationPage
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <div className="max-w-2xl mx-auto">
+        <Card className="border-emerald-900/20">
+          <CardHeader className="text-center">
+            <div
+              className={`mx-auto p-4 ${
+                isRejected ? "bg-red-900/20" : "bg-amber-900/20"
+              } rounded-full mb-4 w-fit`}
+            >
+              {isRejected ? (
+                <XCircle className="h-8 w-8 text-red-400" />
+              ) : (
+                <ClipboardCheck className="h-8 w-8 text-amber-400" />
+              )}
+            </div>
+            <CardTitle>
+              {isRejected
+                ? "Verification Declined"
+                : "Verification in Progress"}
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default VerificationPage;
